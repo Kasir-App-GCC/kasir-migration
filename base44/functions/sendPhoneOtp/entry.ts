7ea982bc99fdc7292a73133d8e067ffbe4bcc0bc
abdulaalm <abdulaalm@hotmail.com>
2026-08-20 14:39:17 +0000

    Revert to SMS OTP provider

--- base44/functions/sendPhoneOtp/entry.ts
+++ base44/functions/sendPhoneOtp/entry.ts
@@ -23,7 +23,7 @@ export default async function(req) {
     const authHeader = 'Basic ' + btoa(`${sid}:${token}`);
     const params = new URLSearchParams();
     params.append('To', phone);
-    params.append('Channel', 'call');
+    params.append('Channel', 'sms');
 
     const twRes = await fetch(`https://verify.twilio.com/v2/Services/${serviceSid}/Verifications`, {
       method: 'POST',
